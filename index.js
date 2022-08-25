f1a5858b5f413f04c05e782c0538751e73df002c
Allan <allan.kowiti@student.moringaschool.com>
2022-08-25 18:18:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const str = word.split("").reverse().join("");
+  if(str=== word){
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
 */
+// function isPalindrome will our solution while the parametre will hold the string we are trying to test for
 
 /*
   Add written explanation of your solution here
 */
+//.split("")method will turn the string into an array, .reverse() will reverse the string, .join("") will convert the array back into a string
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
